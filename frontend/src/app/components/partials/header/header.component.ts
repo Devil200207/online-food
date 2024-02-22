42a522a60eb39b363ffe695a803529748720b568
Devil200207 <74138871+Devil200207@users.noreply.github.com>
2024-02-22 17:28:42 +0530

    logout/login method sucessfuly implemented

--- frontend/src/app/components/partials/header/header.component.ts
+++ frontend/src/app/components/partials/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/shared/models/User';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +9,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+
+  cartQuantity = 0;
+  user!:User;
+
+  constructor(cartService: CartService, private userService: UserService)
+  {
+    cartService.getCartObserable().subscribe((newCart) => {
+      this.cartQuantity = newCart.totalQuantity;
+    })
+
+    userService.userObserable.subscribe((user) => {
+      this.user = user;
+    })
+
+
+  }
+
+  logOut()
+  {
+    this.userService.logout();
+  }
+
+  get isAuth()
+  {
+    return this.user.token;
+  }
 
 }
